6b24cce3bfb729a7ca2349349459163cd79aa25c
Miguel Carlos Honorio <miguel@prosulti.com.br>
2020-01-06 17:51:11 -0300

    Migration de tweets

--- twitter-mongo-adonis/database/migrations/1578343551814_tweet_schema.js
+++ twitter-mongo-adonis/database/migrations/1578343551814_tweet_schema.js
@@ -4,14 +4,23 @@
 const Schema = use('Schema')
 
 class TweetSchema extends Schema {
-  up () {
+  up() {
     this.create('tweets', (table) => {
       table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('content', 240).notNullable()
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('tweets')
   }
 }
